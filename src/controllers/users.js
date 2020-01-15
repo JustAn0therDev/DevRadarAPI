6b49b59fee04b2fe3b3f58951a3a36775e4bddd3
Ruan Montelo <ruanmontelo@gmail.com>
2020-01-14 23:37:19 -0300

    Excluindo arquivo users.js

--- src/controllers/users.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-const router = Router();
-
-//Query Params: Parâmetros enviados pela URL, acessados por: req.query
-//Route Params: Parâmetro enviados diretamente na rota. Ex: /users/:id. Acessados por: req.params
-//Body: Corpo da requisição. Acessado por: req.body
-
-
-export default router;
